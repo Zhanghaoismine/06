77d99db88c1fbfadf399620ecd97891c245906a1
Zhanghaoismine <kegberry@naver.com>
2023-10-12 16:13:38 +0900

    3

--- main.c
+++ main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func1(void) {
-	int x;
-	printf("func1 x is at %p\n", &x);
+int sumTwo(int a, int b) {
+	return a+b;
 }
-
-int main(void){
-	int x;
-	printf("main x is at %p\n", &x);
-	func1();
+int square(int n){
+	return n*n;
+}
+int get_max(int x, int y) {
+	if (x>y){
+		return x;
+}else{
+	return y;
+}		
+}
+int main(int argc, char *argv[]){
+	int a, b;
+	int n;
+	int x, y;	
+	a=3;
+	b=4;
+	printf("%i\n", sumTwo(a,b));
+	n=5;
+	printf("%i\n", square(n));
+	x=6;
+	y=7;
+	printf("%i\n", get_max(x, y));
 	
 	return 0;
 }
